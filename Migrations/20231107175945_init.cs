20ae887c51e9a2390f760fd05e33f96f0dc3afc0
Vasjen Mukhin <103886550+vasjen@users.noreply.github.com>
2023-11-07 21:06:23 +0300

    Add dockerfile and docker-compose.yaml Rollback to sdk7.0

---
+++ Migrations/20231107175945_init.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace fbtracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    CardId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FbId = table.Column<int>(type: "integer", nullable: false),
+                    FbDataId = table.Column<int>(type: "integer", nullable: false),
+                    ShortName = table.Column<string>(type: "text", nullable: false),
+                    Raiting = table.Column<int>(type: "integer", nullable: false),
+                    Version = table.Column<string>(type: "text", nullable: false),
+                    Position = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<string>(type: "text", nullable: false),
+                    Tradable = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.CardId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CardId = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<int>(type: "integer", nullable: false),
+                    SellPrice = table.Column<int>(type: "integer", nullable: false),
+                    ProfitValue = table.Column<int>(type: "integer", nullable: false),
+                    Percentage = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profits_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "CardId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profits_CardId",
+                table: "Profits",
+                column: "CardId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profits");
+
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
