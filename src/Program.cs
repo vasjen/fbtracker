82e78941ba66b6719b08d746ddcbb042570e89ee
Vasjen Mukhin <103886550+vasjen@users.noreply.github.com>
2024-02-04 17:53:39 +0300

    Attempt to add Loki + Grafana

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,9 @@
 using Discord;
 using Discord.WebSocket;
-using fbtracker.Models;
 using fbtracker.Services;
 using fbtracker.Services.Interfaces;
+using Serilog;
+using Serilog.Sinks.Loki;
 using Telegram.Bot;
 
 namespace fbtracker{
@@ -10,55 +11,61 @@ namespace fbtracker{
 internal class Program
 {
     private static async Task Main(string[] args)
-    { 
-        IHost host = new HostBuilder()
-            .ConfigureServices( services =>
-            { 
-                services.AddTransient<IConfiguration>(sp => 
-                { 
-                    IConfigurationBuilder configurationBuilder = new ConfigurationBuilder(); 
-                    configurationBuilder.AddJsonFile("appsettings.json")
-                                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                                    .AddUserSecrets<Program>();
-                    return configurationBuilder.Build();
-                });
-                services.AddTransient<IWebService, WebService>();
-                services.AddHttpClient("proxy",options =>
-                {
-                    IConfiguration configuration = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
-                    string? apikey = configuration.GetValue<string>("Proxy:API");
-                    options.BaseAddress = new Uri(configuration.GetValue<string>("Proxy:Service") + apikey) ?? throw new ArgumentNullException(nameof(options.BaseAddress));
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddTransient<IConfiguration>(sp => 
+        { 
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder(); 
+            configurationBuilder.AddJsonFile("appsettings.json")
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddUserSecrets<Program>();
+            return configurationBuilder.Build();
+        });
+        builder.Services.AddTransient<IWebService, WebService>();
+        builder.Services.AddHttpClient("proxy",options =>
+        {
+            IConfiguration configuration = builder.Services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+            string? apikey = configuration.GetValue<string>("Proxy:API");
+            options.BaseAddress = new Uri(configuration.GetValue<string>("Proxy:Service") + apikey) ?? throw new ArgumentNullException(nameof(options.BaseAddress));
                     
-                });
-                services.AddTransient<IPriceService, PriceService>();
-                services.AddScoped<SeedData>();
-                services.AddTransient<IProfitService, ProfitService>();
-                services.AddTransient<ISalesHistoryService,SalesHistoryService>();
-                services.AddTransient<ITelegramService,TelegramService>();
-                services.AddTransient<INotificationService,DiscordService>();
-                services.AddTransient<IImageService,ImageService>();
-                services.AddSingleton<DiscordSocketClient>(p => 
-                {
-                    DiscordSocketClient client = new DiscordSocketClient();
-                    string? token = p.GetRequiredService<IConfiguration>().GetValue<string>("Discord:Token");
-                    client.LoginAsync(TokenType.Bot, token);
-                    client.StartAsync();
-                    return client;
-                });
+        });
+        builder.Services.AddTransient<IPriceService, PriceService>();
+        builder.Services.AddScoped<SeedData>();
+        builder.Services.AddTransient<IProfitService, ProfitService>();
+        builder.Services.AddTransient<ISalesHistoryService,SalesHistoryService>();
+        builder.Services.AddTransient<ITelegramService,TelegramService>();
+        builder.Services.AddTransient<INotificationService,DiscordService>();
+        builder.Services.AddTransient<IImageService,ImageService>();
+        builder.Services.AddSingleton<DiscordSocketClient>(p => 
+        {
+            DiscordSocketClient client = new DiscordSocketClient();
+            string? token = p.GetRequiredService<IConfiguration>().GetValue<string>("Discord:Token");
+            client.LoginAsync(TokenType.Bot, token);
+            client.StartAsync();
+            return client;
+        });
                 
-                services.AddSingleton<ITelegramBotClient,TelegramBotClient>( p=>
-                {
-                    string? token = p.GetService<IConfiguration>().GetValue<string>("Telegram:Token");
-                    return new TelegramBotClient(token: token);
-                });
-                services.AddHostedService<PriceCheckerBackground>();
-                services.AddLogging(p =>
-                {
-                    p.AddConsole();
-                });
-       })
-        .Build();
-        await host.RunAsync();
+        builder.Services.AddSingleton<ITelegramBotClient,TelegramBotClient>( p=>
+        {
+            string? token = p.GetService<IConfiguration>().GetValue<string>("Telegram:Token");
+            return new TelegramBotClient(token: token);
+        });
+        builder.Services.AddHostedService<PriceCheckerBackground>();
+        
+        builder.Services.AddLogging(p =>
+        {
+            p.AddSerilog();
+        });
+        builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
+        {
+            loggerConfiguration
+                .ReadFrom.Configuration(hostingContext.Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.LokiHttp(new NoAuthCredentials("http://loki"));
+        });
+        WebApplication app = builder.Build();
+        await app.RunAsync();
     }
     
 }
